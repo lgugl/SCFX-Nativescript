4956ae074c87267872e2e8c02b9f2e867adb1039
Loïc Guglielmino <polpoy19@gmail.com>
2018-01-28 16:59:58 +0100

    preload and play sound
    note: it's quite slow, need to preload all the sound files during app's start

--- src/app/pages/unit/unit.component.ts
+++ src/app/pages/unit/unit.component.ts
@@ -5,6 +5,8 @@ import { Unit } from "./../../shared/unit";
 import { UnitService } from "./../../shared/unit.service";
 import { Sound } from "./../../shared/sound";
 
+var SoundModule = require("nativescript-sound");
+
 @Component({
     selector: "unit",
     moduleId: module.id,
@@ -14,6 +16,7 @@ export class UnitComponent implements OnInit {
     faction: string;
     unit: Unit;
     sounds: Array<Sound>;
+    soundFiles = {};
 
     constructor(
         private route: ActivatedRoute,
@@ -25,5 +28,12 @@ export class UnitComponent implements OnInit {
         this.faction = this.route.snapshot.params['faction'];
         this.unit = this.unitService.loadFaction(this.faction).getUnit(id);
         this.sounds = this.unit.sounds;
+        this.sounds.forEach(sound => {
+            this.soundFiles[sound.id] = SoundModule.create('~/assets/sounds/' + sound.sound + '.ogg');
+        });
+    }
+
+    playSound(soundId: string) {
+        this.soundFiles[soundId].play();
     }
 }
