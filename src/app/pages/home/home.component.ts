52ada74f5c6e7f05f4f230863ca7b7d80d32e2a9
Loïc Guglielmino <polpoy19@gmail.com>
2018-10-14 15:13:16 +0200

    some css tests

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,33 @@
 import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { EventData } from "ui/page";
 
 @Component({
     selector: "home",
-    templateUrl: "./pages/home/home.html"
+    templateUrl: "./pages/home/home.html",
+    styleUrls: ["./css/home.css"]
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    private factButton: any;
+
+    constructor(
+        private router: Router
+    ) {
+        router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                if (this.factButton) {
+                    setTimeout(function() {
+                        this.factButton.deletePseudoClass("active");
+                    }.bind(this), 0);
+                }
+            }
+
+        });
+    }
+
+    onFactButtonTap(event: EventData): void {
+        this.factButton = event.object;
+        this.factButton.addPseudoClass("active");
+    }
+}
