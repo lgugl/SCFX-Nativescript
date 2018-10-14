26532eba910c77d6ee16d155616a68f4e306c402
Loïc Guglielmino <polpoy19@gmail.com>
2018-10-14 15:09:54 +0200

    remove action bar hidden on the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { Page } from "ui/page";
 
 @Component({
     selector: "main",
     template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-    constructor(private page:Page) {}
-    ngOnInit(): void {
-        this.page.actionBarHidden = true;
-    }
-}
+export class AppComponent {}
